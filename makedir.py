2c1b001c5b74dec3b39589cd71b0f7f4be0b16d8
ss141309 <lumarzeli30@gmail.com>
2020-06-07 17:04:51 +0530

    Update makedir.py

--- makedir.py
+++ makedir.py
@@ -1,12 +1,14 @@
 import os
 import glob
 
+
 def mkdirs(dir):
 
-    os.makedirs(dir, exist_ok = True) # exist_ok: if the folder already exists it will not throw an error
+    # exist_ok: if the folder already exists it will not throw an error
+    os.makedirs(dir, exist_ok=True)
 
 
-def cleanup(dir): # https://stackoverflow.com/questions/185936/how-to-delete-the-contents-of-a-folder
+def cleanup(dir):  # https://stackoverflow.com/questions/185936/how-to-delete-the-contents-of-a-folder
     mypath = os.path.join(dir, '*.jpg')
     files = glob.glob(mypath)
     for f in files:
